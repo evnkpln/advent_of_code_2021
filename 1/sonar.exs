4cf23b469cce8bb0df8159d775678e6dc19f8b96
Evan Kaplan <36173025+evnkpln@users.noreply.github.com>
2021-12-04 01:39:36 -0500

    Format solutions.

--- 1/sonar.exs
+++ 1/sonar.exs
@@ -1,44 +1,56 @@
 defmodule Sonar do
   def increases([]), do: 0
   def increases([depth | tail]), do: increases(tail, depth)
+
   def increases(file_path) when is_bitstring(file_path) do
     file_path
-      |> parse_readings
-      |> increases
+    |> parse_readings
+    |> increases
   end
 
   def increases([], _last_depth), do: 0
+
   def increases([depth | tail], last_depth) when depth > last_depth do
     1 + increases(tail, depth)
   end
+
   def increases([depth | tail], _last_depth), do: increases(tail, depth)
 
   def parse_readings(file_path) do
     {:ok, content} = File.read(file_path)
+
     content
-      |> String.trim
-      |> String.split
-      |> Enum.map(&(String.to_integer(&1)))
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer(&1))
   end
 
   def stable_increases([]), do: 0
   def stable_increases([depth | tail]), do: stable_increases(tail, [depth])
+
   def stable_increases(file_path) when is_bitstring(file_path) do
     file_path
-      |> parse_readings
-      |> stable_increases
+    |> parse_readings
+    |> stable_increases
   end
 
   def stable_increases([], _window), do: 0
+
   def stable_increases([depth | tail], window) do
-    case length window do
-      1 -> stable_increases(tail, [depth | window])
-      2 -> stable_increases(tail, [depth | window])
+    case length(window) do
+      1 ->
+        stable_increases(tail, [depth | window])
+
+      2 ->
+        stable_increases(tail, [depth | window])
+
       3 ->
-        new_window = window
+        new_window =
+          window
           |> List.delete_at(2)
           |> List.insert_at(0, depth)
-        compare_windows(window,new_window) + stable_increases(tail, new_window)
+
+        compare_windows(window, new_window) + stable_increases(tail, new_window)
     end
   end
 
--- 2/helm.exs
+++ 2/helm.exs
@@ -2,6 +2,7 @@ defmodule Helm do
   defmodule Coordinates do
     defstruct depth: 0, horizontal: 0
   end
+
   defmodule Heading do
     defstruct depth: 0, horizontal: 0, aim: 0
   end
@@ -21,27 +22,32 @@ defmodule Helm do
       :forward -> %{coordinates | horizontal: coordinates.horizontal + distance}
     end
   end
+
   def move({direction, distance}, heading = %Heading{}) do
     case direction do
-      :up -> %{heading | aim: heading.aim - distance}
-      :down -> %{heading | aim: heading.aim + distance}
+      :up ->
+        %{heading | aim: heading.aim - distance}
+
+      :down ->
+        %{heading | aim: heading.aim + distance}
+
       :forward ->
         %{
           heading
-          |
-          horizontal: heading.horizontal + distance,
-          depth: heading.depth + (distance * heading.aim)
+          | horizontal: heading.horizontal + distance,
+            depth: heading.depth + distance * heading.aim
         }
     end
   end
 
   def parse_instructions(file_path) do
     {:ok, content} = File.read(file_path)
+
     content
-      |> String.trim
-      |> String.split("\n")
-      |> Enum.map(&(String.split(&1)))
-      |> Enum.map(&(list_to_move_tuple(&1)))
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1))
+    |> Enum.map(&list_to_move_tuple(&1))
   end
 
   def apply_instructions(coordinates, instructions) do
@@ -55,14 +61,16 @@ defmodule Helm do
   end
 end
 
-#Part 1
+# Part 1
 instructions = Helm.parse_instructions("input.txt")
+
 Helm.initialize_position()
-  |> Helm.apply_instructions(instructions)
-  |> Helm.area_covered()
-  |> IO.puts()
-#Part 2
+|> Helm.apply_instructions(instructions)
+|> Helm.area_covered()
+|> IO.puts()
+
+# Part 2
 Helm.initialize_heading()
-  |> Helm.apply_instructions(instructions)
-  |> Helm.area_covered()
-  |> IO.puts()
+|> Helm.apply_instructions(instructions)
+|> Helm.area_covered()
+|> IO.puts()
